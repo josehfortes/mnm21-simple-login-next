3fe0235d2b389053fa33585a16380ca256a6c3ec
Jose Fortes <vasco0007@gmail.com>
2022-12-14 10:00:56 -0300

    Created services and jwt

--- services/user.js
+++ services/user.js
@@ -1,18 +1,39 @@
+import jwt from 'jsonwebtoken'
 let users = []
+
+const SECRET = process.env.JWT_SECRET
+
+function generateToken(user) {
+  return jwt.sign({ name: user.name, email: user.email }, SECRET)
+}
+
+function readToken(token) {
+  try {
+    return jwt.verify(token, SECRET)
+  } catch (err) {
+    throw new ('token_invalido')
+  }
+}
 
 export function cadastro(body) {
   const user = users.find(({ email }) => email === body.email)
   if (user) throw new Error('usuario_ja_cadastrado')
 
-  users.push(body)
-  return body
+  users.push(body) //substituir por um BD
+
+  const token = generateToken(body)
+  return token
 }
 
 export function login(body) {
-  const user = users.find(({ email }) => email === body.email)
+  const user = users.find(({ email }) => email === body.email) //substituir por um bd
   if (!user) throw new Error('usuario_nao_encontrado')
   if (user.password !== body.password) throw new Error('senha_incorreta')
 
-  return user
+  const token = generateToken(user)
+  return token
 }
 
+export function verifica(token) {
+  return readToken(token)
+}
